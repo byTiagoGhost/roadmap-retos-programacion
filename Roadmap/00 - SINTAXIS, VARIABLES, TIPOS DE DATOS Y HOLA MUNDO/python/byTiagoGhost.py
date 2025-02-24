ddf55adf6c2e403636b7fe4287cab5406c4d2f08
Santiago <125517926+byTiagoGhost@users.noreply.github.com>
2025-02-24 17:42:22 -0500

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/byTiagoGhost.py
@@ -0,0 +1,53 @@
+# URL de Python: https://www.python.org/ 
+
+# Esto es un comentario de una sola Linea de Codigo en Python
+
+"""
+Esto es un comentario 
+de varias Lineas de Codigo 
+en Python
+"""
+
+# Variables en Python
+x = 5 # Esto representa una variable de tipo entero 
+print(x)
+y = "Santiago" # Esto representa una variable de tipo cadena de texto
+print(y)
+z = 3.5 # Esto representa una variable de tipo flotante
+print(z)
+w = True # Esto representa una variable de tipo booleano
+print(w)
+x, y, z = "Santiago", "Tiago", "Ghost" # Esto representa varias variables de tipo cadena de texto en una sola linea de Codigo
+print(x)
+print(y)
+print(z)
+
+# Y tambien puedo dejarlas ya especificadas o mas bien dicho ya declaradas
+a = str(3) # Esto representa una variable de tipo cadena de texto en numero
+print(a)
+a = str("Tiago") # Y esto con texto representa una variable de tipo cadena de texto
+print(a)
+b = int(3) # Esto representa una variable de tipo entero
+print(b)
+c = float(3) # Esto representa una variable de tipo flotante
+print(c)
+
+"""
+Las letras reperesenta las varibles 
+Mientras que los numeros y los textos funcionan como 
+valores o constantes.      
+"""
+
+# El output para los codigos desde la consola seria el siguiente: 
+
+x = "Estoy aprendiendo Python gracias a MoureDev"
+print(x) #Aqui lo represento en sola linea de codigo
+
+x = "Estoy aprendiendo"
+y = "Python"
+z = "gracias a MoureDev"
+print(x, y, z) # O los separo en varias lineas y los uno en este print
+
+# Y por ultimo 
+x = "Python"
+print("¡Hola", x, "!")
